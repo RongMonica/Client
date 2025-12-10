199bb21d412ebf7dd016189d3523f89134097697
RongMonica <rongqingqing3038@gmail.com>
2025-12-09 23:18:29 -0600

    define BUFFER_SIZE and update CMakeLists

--- client.cpp
+++ client.cpp
@@ -1,25 +1,26 @@
- #include <arpa/inet.h>
-#include <stdio.h>
+#include <arpa/inet.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 #include <iostream>
 #include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <array>
 using namespace std;
 
-#define PORT 8080
+#define PORT 19879
+#define BUFFER_SIZE 1024
 
 int main(int argc, char const* argv[])
-{
-    int status, valread, client_fd;
-    struct sockaddr_in serv_addr;
-    char buffer[1024] = { 0 };
+{   
+    int client_fd;
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket");
         return EXIT_FAILURE;
     }
-
+    struct sockaddr_in serv_addr{};
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT); //convert from machines's byte order-->network byte order "host-to-network-short"
 
@@ -29,17 +30,21 @@ int main(int argc, char const* argv[])
         return EXIT_FAILURE;
     }
 
+    //connect to server
+    int status;
     if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
         perror("Connection failed");
         return EXIT_FAILURE;
     }
 
+    char buffer[BUFFER_SIZE] = { 0 };
     while(1){
-        string require;
-        getline(cin, require);
-        const char *hello = require.c_str();
-        send(client_fd, hello, require.size(), 0); 
-        valread = read(client_fd, buffer, sizeof(buffer) - 1); // subtract 1 for the null
+        string request;
+        getline(cin, request);
+        if(send(client_fd, request.c_str(), request.size(), 0) < 0){
+            perror("Send failed");
+        }
+        ssize_t valread = read(client_fd, buffer, BUFFER_SIZE - 1); // subtract 1 for the null
         if(valread == 0){
             cout << "Server closed connection!!!" << endl;
             close(client_fd);
@@ -50,7 +55,7 @@ int main(int argc, char const* argv[])
         }else{
             buffer[valread] = '\0';
             cout << buffer << endl;
-            memset(buffer, 0, sizeof(buffer));
+            memset(buffer, 0, BUFFER_SIZE);
         }
     }
     // closing the connected socket
